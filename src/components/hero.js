a75d84f606bb5002d5937e2e54f32ef62810adf0
Alex Dudley <alexdudman@gmail.com>
2019-07-20 17:42:21 +0100

    Hero styling now looks okay. Added in Tailwind CSS breakpoints

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,13 +4,11 @@ import LogoColor from "../images/logo-color.svg"
 
 const Hero = () => {
     return (
-        <div className="hero-outer flex items-center w-full bg-black">
-            <div style={{ backgroundImage: "url(" + Ellipse + ")" }} className="hero-inner text-white w-full text-center flex justify-start self-center bg-no-repeat bg-left h-full">
-                <div className="hero-icon flex">
-                    <img src={LogoColor} />
-                </div>
-                <div className="hero-text flex flex-col self-center">
-                    Alex James <span className="block">Web Designer // Front-End Developer Nottingham</span>
+        <div className="hero-outer flex items-center w-full bg-black bg-no-repeat justify-center vh-9" style={{ backgroundImage: "url(" + Ellipse + ")" }}>
+            <div className="hero-inner text-white w-full text-center flex flex-wrap md:flex-no-wrap justify-center md:justify-start content-center h-full">
+                <img className="hero-icon s:w-1/4 m:width-48" src={LogoColor} />
+                <div className="hero-text flex flex-col self-center p-2 md:p-0 md:pl-8 md:pb-5 w-full md:w-auto">
+                    <h1 className="width-1/4 text-5xl md:text-6xl text-center md:text-left">Alex James <span className="block text-xl text-center md:text-left">Web Designer // <span className="block text-xl">Front-End Developer Nottingham</span></span></h1>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  theme: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1400px',
+    },
+  },
   variants: {},
   plugins: [],
 }
