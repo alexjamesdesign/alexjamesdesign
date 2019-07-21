027064ef959b7824421df759c64af2f3d430fabe
Alex Dudley <alexdudman@gmail.com>
2019-07-21 21:18:41 +0100

    Made progress creating the component for Designer Developer and passing props though. Also some basic styling

--- src/components/designer-developer.js
+++ src/components/designer-developer.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Img from "gatsby-image"
 
-const DesignerDeveloper = () => {
+const DesignerDeveloper = (props) => {
     return (
-        <div className="designer-developer-outer flex items-center w-full bg-black bg-no-repeat justify-center">
-            HELLO WORLD
+        <div className={`designer-developer-outer flex w-full bg-white flex flex-wrap m-4 ${props.position}`}>
+            <div className="dd-text lg:w-3/4">
+                <p className="title text-5xl md:text-6xl text-black">{props.title}</p>
+                <p className="main-content">{props.content}</p>
+            </div>
+            
+            <div style={{ backgroundImage: `url(${props.backgroundImage})` }} className="dd-img lg:w-1/4 bg-black">
+
+            </div>
         </div>  
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,33 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import DesignerDeveloper from "../components/designer-developer"
 
+import DesignerBgImage from "../images/gatsby-astronaut.png"
+
+console.log(DesignerBgImage)
+
 const IndexPage = props => (
+  
   <Layout>
     <SEO title="Home" />
 
     <Hero />
-    <DesignerDeveloper />
+
+    <DesignerDeveloper
+      position="right"
+      title="Designer"
+      backgroundImage="DesignerBgImage`"
+      
+      content="I have a passion for all things creative, from interior design through to web design. I’m experienced in web design project serving both the UK and US in a range of different sectors."
+    />
+
+    <DesignerDeveloper
+      position="left"
+      title="Developer"
+      backgroundImage="DeveloperBgImage"
+      content="I have experience in creating custom WordPress themes with speed and accessability in mind. I use Git version control to colaborate on projects across the company I currently work as well an using SCSS and jQuery to enhance UI elements.
+
+      I’ve been teaching myself React JS in my spare time as well as experimenting with Gatsby, Netlify and other web technologies to progress my skillset."
+    />
 
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -26,3 +47,21 @@ const IndexPage = props => (
 
 export default IndexPage
 
+export const pageQuery = graphql`
+  query {
+    designerBgImage: file(relativePath: { eq: "gatsby-icon.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    developerBgImage: file(relativePath: { eq: "gatsby-icon.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
