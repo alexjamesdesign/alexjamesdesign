1f8977ef5bb9f89b2c05ec87fbff4e9a781a9b73
Alex Dudley <alexdudman@gmail.com>
2019-07-23 00:03:24 +0100

    Styled some more for mobile

--- src/components/designer-developer.js
+++ src/components/designer-developer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const DesignerDeveloper = (props) => {
     return (
-        <div className={`designer-developer-outer container flex w-full flex flex-wrap mb-20 ${props.position}`}>
+        <div className={`designer-developer-outer container flex w-full flex flex-wrap mb-20 tablet:mb-0 ${props.position}`}>
             <div className="dd-text tablet:w-3/4">
                 <p className="title text-5xl md:text-6xl text-black">{props.title}</p>
                 <p className="main-content">{props.content}</p>
             </div>
             
-            <div className="dd-img tablet:w-1/4" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+            <div className="dd-img w-full tablet:w-1/4 h-64 tablet:h-auto" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
                 
             </div>
         </div>  
