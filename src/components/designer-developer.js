624360c91c638db4fb238ba5ba6cc4d199b3d941
Alex Dudley <alexdudman@gmail.com>
2019-07-22 19:06:50 +0100

    Trying to get the background-image working

--- src/components/designer-developer.js
+++ src/components/designer-developer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+console.log(designerBgImage)
 
 const DesignerDeveloper = (props) => {
     return (
@@ -9,8 +12,8 @@ const DesignerDeveloper = (props) => {
                 <p className="main-content">{props.content}</p>
             </div>
             
-            <div style={{ backgroundImage: `url(${props.backgroundImage})` }} className="dd-img lg:w-1/4 bg-black">
-
+            <div className="dd-img lg:w-1/4 bg-black" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+                
             </div>
         </div>  
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,6 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import DesignerDeveloper from "../components/designer-developer"
 
-import DesignerBgImage from "../images/gatsby-astronaut.png"
-
-console.log(DesignerBgImage)
-
 const IndexPage = props => (
   
   <Layout>
@@ -23,7 +19,7 @@ const IndexPage = props => (
     <DesignerDeveloper
       position="right"
       title="Designer"
-      backgroundImage="DesignerBgImage`"
+      backgroundImage={props.data.designerBgImage.absolutePath}
       
       content="I have a passion for all things creative, from interior design through to web design. I’m experienced in web design project serving both the UK and US in a range of different sectors."
     />
@@ -31,7 +27,7 @@ const IndexPage = props => (
     <DesignerDeveloper
       position="left"
       title="Developer"
-      backgroundImage="DeveloperBgImage"
+      backgroundImage="developerBgImage"
       content="I have experience in creating custom WordPress themes with speed and accessability in mind. I use Git version control to colaborate on projects across the company I currently work as well an using SCSS and jQuery to enhance UI elements.
 
       I’ve been teaching myself React JS in my spare time as well as experimenting with Gatsby, Netlify and other web technologies to progress my skillset."
@@ -50,13 +46,6 @@ export default IndexPage
 export const pageQuery = graphql`
   query {
     designerBgImage: file(relativePath: { eq: "gatsby-icon.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    developerBgImage: file(relativePath: { eq: "gatsby-icon.png" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
