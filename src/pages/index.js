cd06c4ac382eb197f159d0eaad1b8d84453b14f3
Alex Dudley <alexdudman@gmail.com>
2019-07-22 20:49:29 +0100

    Managed to get images pulling through as a background, finally!!

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import DesignerDeveloper from "../components/designer-developer"
 
+import DesignerImage from "../images/designer-background.jpg"
+import DeveloperImage from "../images/developer-background.jpg"
+
+
 const IndexPage = props => (
   
   <Layout>
@@ -21,7 +25,7 @@ const IndexPage = props => (
       <DesignerDeveloper
         position="right"
         title="Designer"
-        backgroundImage={props.data.designerBgImage}
+        backgroundImage={DesignerImage}
         
         content="I have a passion for all things creative, from interior design through to web design. I’m experienced in web design project serving both the UK and US in a range of different sectors."
       />
@@ -29,7 +33,7 @@ const IndexPage = props => (
       <DesignerDeveloper
         position="left"
         title="Developer"
-        backgroundImage="developerBgImage"
+        backgroundImage={DeveloperImage}
         content="I have experience in creating custom WordPress themes with speed and accessability in mind. I use Git version control to colaborate on projects across the company I currently work as well an using SCSS and jQuery to enhance UI elements.
 
         I’ve been teaching myself React JS in my spare time as well as experimenting with Gatsby, Netlify and other web technologies to progress my skillset."
