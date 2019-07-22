3e5880c62d37dfaf1323f4f6459c45f3cad65467
Alex Dudley <alexdudman@gmail.com>
2019-07-22 20:25:05 +0100

    Build up the styling some more on designer developer component including tidying up padding and margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,22 +16,26 @@ const IndexPage = props => (
 
     <Hero />
 
-    <DesignerDeveloper
-      position="right"
-      title="Designer"
-      backgroundImage={props.data.designerBgImage}
-      
-      content="I have a passion for all things creative, from interior design through to web design. I’m experienced in web design project serving both the UK and US in a range of different sectors."
-    />
+    <div className="designer-developer-container">
 
-    <DesignerDeveloper
-      position="left"
-      title="Developer"
-      backgroundImage="developerBgImage"
-      content="I have experience in creating custom WordPress themes with speed and accessability in mind. I use Git version control to colaborate on projects across the company I currently work as well an using SCSS and jQuery to enhance UI elements.
+      <DesignerDeveloper
+        position="right"
+        title="Designer"
+        backgroundImage={props.data.designerBgImage}
+        
+        content="I have a passion for all things creative, from interior design through to web design. I’m experienced in web design project serving both the UK and US in a range of different sectors."
+      />
 
-      I’ve been teaching myself React JS in my spare time as well as experimenting with Gatsby, Netlify and other web technologies to progress my skillset."
-    />
+      <DesignerDeveloper
+        position="left"
+        title="Developer"
+        backgroundImage="developerBgImage"
+        content="I have experience in creating custom WordPress themes with speed and accessability in mind. I use Git version control to colaborate on projects across the company I currently work as well an using SCSS and jQuery to enhance UI elements.
+
+        I’ve been teaching myself React JS in my spare time as well as experimenting with Gatsby, Netlify and other web technologies to progress my skillset."
+      />
+
+    </div>
 
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
